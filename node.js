51cb459552fc312b5eb77334b5126807bd730ed6
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-11-24 00:36:02 +0200

    handle url module

--- node.js
+++ node.js
@@ -2,10 +2,15 @@ console.log("Hello, Mohamed!");
 
 
 // create  a simple server with node!!
-var http = require('http');
+const http = require('http');
+const url = require('url');
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
+    // handle the url module
+    const que = url.parse(req.url, true).query;
+    const myBirthdate = que.year;
+    res.end('My Birthdate is:' + myBirthdate);
     res.end('Hello, Mohamed! you are a Rockstar :)');
 }).listen(8080);
 
