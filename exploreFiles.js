1a46cb84ad35d33f7329577b983f1f484d6d5077
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-11-24 13:05:07 +0200

    toggle between pages

--- exploreFiles.js
+++ exploreFiles.js
@@ -1,12 +1,24 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 // render a simple index.html page with file system module!
 http.createServer((req, res) => {
-    fs.readFile('index.html', (err, page) => {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(page);
-        res.end();
+    const q = url.parse(req.url, true);
+    // toggle between pages!
+    let fileName = "." + q.pathname;
+    if (fileName == './') {
+        fileName = './index.html';
+    }
+    fs.readFile(fileName, (err, page) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end("404 page not found");
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(page);
+            return res.end();
+        }
     });
 
 }).listen(8080);
